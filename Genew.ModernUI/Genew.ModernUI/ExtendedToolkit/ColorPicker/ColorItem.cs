3bf25f6916f5bbac8d6a11422e0796a02d436a1e
Transfen <jkmchinese@qq.com>
2013-12-23 08:44:35 -0800

    1、增加ColorPicker及样式 2、增加透明笔刷以解决两个颜色选取透明颜色时的显示问题

---
+++ Genew.ModernUI/Genew.ModernUI/ExtendedToolkit/ColorPicker/ColorItem.cs
@@ -0,0 +1,25 @@
+﻿/*******************************************************************
+* 版权所有： 深圳市震有科技有限公司
+* 文件名称： ColorItem.cs
+* 作   者： chenzhifen
+* 创建日期： 2013-12-23 23:30
+* 文件版本： 1.0.0.0
+* 修改时间：             修改人：                修改内容：
+*******************************************************************/
+
+using System.Windows.Media;
+
+namespace Genew.ModernUI.ExtendedToolkit
+{
+    public class ColorItem
+    {
+        public ColorItem(Color color, string name)
+        {
+            Color = color;
+            Name = name;
+        }
+
+        public Color Color { get; set; }
+        public string Name { get; set; }
+    }
+}
