e649e5c85a85e78c183263a52feafc2a35ad9d61
Transfen <jkmchinese@qq.com>
2013-12-23 05:08:55 -0800

    1、增加ColorCanvas控件和样式(样式未完善好)

---
+++ Genew.ModernUI/Genew.ModernUI/ExtendedToolkit/ColorCanvas/ColorSpectrumSlider.cs
@@ -0,0 +1,101 @@
+﻿/*******************************************************************
+* 版权所有： 深圳市震有科技有限公司
+* 文件名称： ColorSpectrumSlider.cs
+* 作   者： chenzhifen
+* 创建日期： 2013-12-23 9:36
+* 文件版本： 1.0.0.0
+* 修改时间：             修改人：                修改内容：
+*******************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Genew.ModernUI.ExtendedToolkit.Utilities;
+
+namespace Genew.ModernUI.ExtendedToolkit
+{
+    [TemplatePart(Name = PART_SpectrumDisplay, Type = typeof(Rectangle))]
+    public class ColorSpectrumSlider : Slider
+    {
+        private const string PART_SpectrumDisplay = "PART_SpectrumDisplay";
+
+        #region Private Members
+
+        private LinearGradientBrush _pickerBrush;
+        private Rectangle _spectrumDisplay;
+
+        #endregion //Private Members
+
+        #region Constructors
+
+        static ColorSpectrumSlider()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorSpectrumSlider),
+                new FrameworkPropertyMetadata(typeof(ColorSpectrumSlider)));
+        }
+
+        #endregion //Constructors
+
+        #region Dependency Properties
+
+        public static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register("SelectedColor",
+            typeof(Color), typeof(ColorSpectrumSlider), new PropertyMetadata(System.Windows.Media.Colors.Transparent));
+
+        public Color SelectedColor
+        {
+            get { return (Color)GetValue(SelectedColorProperty); }
+            set { SetValue(SelectedColorProperty, value); }
+        }
+
+        #endregion //Dependency Properties
+
+        #region Base Class Overrides
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _spectrumDisplay = (Rectangle)GetTemplateChild(PART_SpectrumDisplay);
+            CreateSpectrum();
+            OnValueChanged(Double.NaN, Value);
+        }
+
+        protected override void OnValueChanged(double oldValue, double newValue)
+        {
+            base.OnValueChanged(oldValue, newValue);
+
+            Color color = ColorUtilities.ConvertHsvToRgb(360 - newValue, 1, 1);
+            SelectedColor = color;
+        }
+
+        #endregion //Base Class Overrides
+
+        #region Methods
+
+        private void CreateSpectrum()
+        {
+            _pickerBrush = new LinearGradientBrush();
+            _pickerBrush.StartPoint = new Point(0.5, 0);
+            _pickerBrush.EndPoint = new Point(0.5, 1);
+            _pickerBrush.ColorInterpolationMode = ColorInterpolationMode.SRgbLinearInterpolation;
+
+            List<Color> colorsList = ColorUtilities.GenerateHsvSpectrum();
+
+            double stopIncrement = (double)1 / colorsList.Count;
+
+            int i;
+            for (i = 0; i < colorsList.Count; i++)
+            {
+                _pickerBrush.GradientStops.Add(new GradientStop(colorsList[i], i * stopIncrement));
+            }
+
+            _pickerBrush.GradientStops[i - 1].Offset = 1.0;
+            _spectrumDisplay.Fill = _pickerBrush;
+        }
+
+        #endregion //Methods
+    }
+}
---
+++ Genew.ModernUI/Genew.ModernUI/ExtendedToolkit/Core/Primitives/HsvColor.cs
@@ -0,0 +1,25 @@
+﻿/*******************************************************************
+* 版权所有： 深圳市震有科技有限公司
+* 文件名称： HsvColor.cs
+* 作   者： chenzhifen
+* 创建日期： 2013-12-23 9:49
+* 文件版本： 1.0.0.0
+* 修改时间：             修改人：                修改内容：
+*******************************************************************/
+
+namespace Genew.ModernUI.ExtendedToolkit.Primitives
+{
+    internal struct HsvColor
+    {
+        public double H;
+        public double S;
+        public double V;
+
+        public HsvColor(double h, double s, double v)
+        {
+            H = h;
+            S = s;
+            V = v;
+        }
+    }
+}
