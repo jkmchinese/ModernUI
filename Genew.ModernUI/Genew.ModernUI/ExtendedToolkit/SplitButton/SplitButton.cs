1862254b7c83b7e446258f72b68c955308aae3d6
Transfen <jkmchinese@qq.com>
2013-12-24 05:03:36 -0800

    1、增加Chromes控件及样式 2、增加DropDownButton及样式与SplitButton及样式(未测试) 3、主题笔刷增加Chromes笔刷

---
+++ Genew.ModernUI/Genew.ModernUI/ExtendedToolkit/SplitButton/SplitButton.cs
@@ -0,0 +1,40 @@
+﻿/*******************************************************************
+* 版权所有： 深圳市震有科技有限公司
+* 文件名称： SplitButton.cs
+* 作   者： chenzhifen
+* 创建日期： 2013-12-24 20:53
+* 文件版本： 1.0.0.0
+* 修改时间：             修改人：                修改内容：
+*******************************************************************/
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Genew.ModernUI.ExtendedToolkit
+{
+    [TemplatePart(Name = PART_ActionButton, Type = typeof(Button))]
+    public class SplitButton : DropDownButton
+    {
+        private const string PART_ActionButton = "PART_ActionButton";
+
+        #region Constructors
+
+        static SplitButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SplitButton),
+                new FrameworkPropertyMetadata(typeof(SplitButton)));
+        }
+
+        #endregion //Constructors
+
+        #region Base Class Overrides
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            Button = GetTemplateChild(PART_ActionButton) as Button;
+        }
+
+        #endregion //Base Class Overrides
+    }
+}
